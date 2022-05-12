ca71d04c37044a00fc4b0ee50350f2241bfc0987
Haley Telega <haleytelega@outlook.com>
2022-05-12 18:42:00 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -7,14 +7,13 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
-    } else {
+} else {
 //create connection to our database, pass in your MySQL information for username and password
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-    }
-
-});
+    });
+};
 
 module.exports = sequelize;
